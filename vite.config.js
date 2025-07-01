99528f1915164ea1dda294cb3228372535e0fcfe
breck <breckc@knowbe4.com>
2025-07-01 12:30:09 -0500

    reverting and trying Hashrouter

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import fs from 'fs/promises';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Get directory name equivalent in ESM
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-// Plugin to create _redirects file
-function createRedirectsPlugin() {
-  return {
-    name: 'create-redirects-file',
-    async closeBundle() {
-      const redirectsContent = '/* /index.html 200';
-      const distDir = path.resolve(__dirname, 'dist');
-      
-      try {
-        await fs.writeFile(path.join(distDir, '_redirects'), redirectsContent);
-        console.log('Created _redirects file in dist directory');
-      } catch (error) {
-        console.error('Error creating _redirects file:', error);
-      }
-    }
-  };
-}
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    createRedirectsPlugin()
-  ]
-});
-
-
+  plugins: [react()],
+})
